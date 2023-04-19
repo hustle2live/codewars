2e942e0086cc4400370b8834e70670e2b6b09069
hustle2live <kuz.v@ukr.net>
2023-04-19 17:19:02 +0300

    shorted function lenght to 233

--- 7-kyu/baby-shark-lyrics-generator.js
+++ 7-kyu/baby-shark-lyrics-generator.js
@@ -1,31 +1,13 @@
 // Baby Shark Lyrics
-// baby shark
 
 // Create a function, as short as possible, that returns this lyrics.
-// Your code should be less than 300 characters. Watch out for the three points at the end of the song.
+// Your code should be less than 300 characters. 
+// Watch out for the three points at the end of the song.
 
-const babySharkLyrics = () => {
-   const [dodo, arr, repeat, final] = [
-      ', doo doo doo doo doo doo\n',
-      ['Baby shark', 'Mommy shark', 'Daddy shark', 'Grandma shark', 'Grandpa shark', "Let's go hunt", 'Run away,…'],
-      (name) => name + dodo,
-      (str) => `${str}!\n`
-   ];
-   let song = '';
-   for (let k = 0; k < arr.length; k++) {
-      if (k === arr.length - 1) song += arr[arr.length - 1];
-      else
-         for (let i = 0; i < 4; i++) {
-            song += i === 3 ? final(arr[k]) : repeat(arr[k]);
-         }
-   }
-   return song;
-};
+// const expectedRsult = "Baby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark!\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark!\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark!\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark!\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark!\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt!\nRun away,…";
 
-const expected =
-   "Baby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark!\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark!\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark!\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark!\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark!\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt!\nRun away,…";
-
-// babySharkLyrics();
-
-console.log(babySharkLyrics() === expected);
-console.log(babySharkLyrics.toString().length);
+const babySharkLyrics = () =>
+   ['Baby shark', 'Mommy shark', 'Daddy shark', 'Grandma shark', 'Grandpa shark', "Let's go hunt"].reduce(
+      (acc, name) => (acc += `${name},${' doo'.repeat(6)}\n`.repeat(3) + `${name}!\n`),
+      ''
+   ) + 'Run away,…';
