e36cbbd9b9f37d0d10940008e1fe53e844f0bfda
hustle2live <kuz.v@ukr.net>
2023-04-19 15:55:29 +0300

    moderate functions

--- 7-kyu/baby-shark-lyrics-generator.js
+++ 7-kyu/baby-shark-lyrics-generator.js
@@ -4,7 +4,7 @@
 // Create a function, as short as possible, that returns this lyrics.
 // Your code should be less than 300 characters. Watch out for the three points at the end of the song.
 
-function babySharkLyrics() {
+const babySharkLyrics = () => {
    const [dodo, arr, repeat, final] = [
       ', doo doo doo doo doo doo\n',
       ['Baby shark', 'Mommy shark', 'Daddy shark', 'Grandma shark', 'Grandpa shark', "Let's go hunt", 'Run away,…'],
@@ -19,9 +19,8 @@ function babySharkLyrics() {
             song += i === 3 ? final(arr[k]) : repeat(arr[k]);
          }
    }
-   // console.log(song);
    return song;
-}
+};
 
 const expected =
    "Baby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark, doo doo doo doo doo doo\nBaby shark!\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark, doo doo doo doo doo doo\nMommy shark!\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark, doo doo doo doo doo doo\nDaddy shark!\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark, doo doo doo doo doo doo\nGrandma shark!\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark, doo doo doo doo doo doo\nGrandpa shark!\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt, doo doo doo doo doo doo\nLet's go hunt!\nRun away,…";
