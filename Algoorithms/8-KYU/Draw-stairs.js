ef8f88b49864ea4528ea3de72092deafc456bf16
hustle2live <kuz.v@ukr.net>
2023-05-03 09:33:31 +0300

    draw stairs add

---
+++ Algoorithms/8-KYU/Draw-stairs.js
@@ -0,0 +1,28 @@
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+// For example n = 3 result in:
+
+// "I\n I\n  I"
+// or printed:
+
+// I
+//  I
+//   I
+
+// https://www.codewars.com/kata/5b4e779c578c6a898e0005c5/
+
+function drawStairs(n) {
+   let str = '';
+   for (let i = 0; i < n; i++) {
+      if (i === n - 1) {
+         str += 'I';
+      } else {
+         str += 'I\n ' + ' '.repeat(i);
+      }
+   }
+   return str;
+}
+
+console.log(drawStairs(1) === 'I');
+console.log(drawStairs(3) === 'I\n I\n  I');
+console.log(drawStairs(5) === 'I\n I\n  I\n   I\n    I');
