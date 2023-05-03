d377cd784154987654fea8305af3744780964345
hustle2live <kuz.v@ukr.net>
2023-05-03 10:44:53 +0300

    cascading subsets

---
+++ Algoorithms/8-KYU/Cascading-Subsets.js
@@ -0,0 +1,31 @@
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+
+// https://www.codewars.com/kata/545af3d185166a3dec001190/
+
+// simple solution
+function eachCons1(array, n, newArr = []) {
+   if (!array.length) return newArr;
+   for (let i = 0; i < array.length; i++) {
+      const subArr = array.slice(i, i + n);
+      subArr.length === n ? newArr.push(subArr) : null;
+   }
+   return newArr;
+}
+
+
+const eachCons = (array, n) =>
+   array.reduce((acc, _, i, arr) => {
+      return arr.length - i >= n ? [...acc, arr.slice(i, i + n)] : acc;
+   }, []);
+
+
+
+
+
+
--- Algoorithms/8-KYU/Draw-stairs.js
+++ Algoorithms/8-KYU/Draw-stairs.js
@@ -23,6 +23,6 @@ function drawStairs(n) {
    return str;
 }
 
-console.log(drawStairs(1) === 'I');
-console.log(drawStairs(3) === 'I\n I\n  I');
-console.log(drawStairs(5) === 'I\n I\n  I\n   I\n    I');
+
+
+
