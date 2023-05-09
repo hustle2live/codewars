657d2b961bbe3d837f9faa10cf7babdd9530f9ff
hustle2live <kuz.v@ukr.net>
2023-05-09 17:22:08 +0300

    square

--- To square(root) or not to square(root).js
+++ To square(root) or not to square(root).js
@@ -1,3 +1,3 @@
 function squareOrSquareRoot(array) {
-    return array.map((n)=> (n % Math.sqrt(n)) === 0 ? Math.sqrt(n) : n**2 );  
-  }
+   return array.map((n) => (n % Math.sqrt(n) === 0 ? Math.sqrt(n) : n ** 2));
+}
