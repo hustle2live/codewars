fa510bb7d97b816e93605e76c4096530d13141ff
hustle2live <kuz.v@ukr.net>
2023-05-09 22:10:39 +0300

    pirates are the cannons

---
+++ Pirates!! Are the Cannons.js
@@ -0,0 +1,17 @@
+// Pirates!! Are the Cannons ready!??
+
+// Your task is to check if the gunners are loaded and ready, if they are: Fire!
+// If they aren't ready: Shiver me timbers!
+// Your gunners for each test case are 2, 3 or 4.
+// When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+// Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+// If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+// Also, check out the new Pirates!! Kata: https://www.codewars.com/kata/57e2d5f473aa6a476b0000fe
+
+
+const cannonsReady = (gunners) => {
+   return Object.values(gunners).every((pirate) => pirate === 'aye') ? 'Fire!' : 'Shiver me timbers!';
+};
+
+
+
