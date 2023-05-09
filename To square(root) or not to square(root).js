93c0fd6464ea2b20c714262ce57256a77ca0d724
hustle2live <kuz.v@ukr.net>
2023-05-09 17:23:27 +0300

    square link

--- To square(root) or not to square(root).js
+++ To square(root) or not to square(root).js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+
 function squareOrSquareRoot(array) {
    return array.map((n) => (n % Math.sqrt(n) === 0 ? Math.sqrt(n) : n ** 2));
 }
