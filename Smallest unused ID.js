05b7306093e8854e264b3e273de8303b30dcf35a
hustle2live <kuz.v@ukr.net>
2023-05-09 17:22:35 +0300

    find ids

--- Smallest unused ID.js
+++ Smallest unused ID.js
@@ -1,9 +1,9 @@
-function nextId(ids){
-  const arr=[];
-  for (let i = 1; i <= ids.length; i++) {
-    if (!ids.find(n => n === i)) arr.push(+i) ;
-  }
-  const minNum = (arr.sort((a,b) => b - a));
+function nextId(ids) {
+   const arr = [];
+   for (let i = 1; i <= ids.length; i++) {
+      if (!ids.find((n) => n === i)) arr.push(+i);
+   }
+   const minNum = arr.sort((a, b) => b - a);
 
-  return minNum.pop()
+   return minNum.pop();
 }
