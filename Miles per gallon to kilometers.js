d890beaefdd640dff192ba31f5592ca162240fe7
hustle2live <kuz.v@ukr.net>
2023-05-09 22:49:10 +0300

    convert miles per gallon to km per litres upgrd

--- Miles per gallon to kilometers.js
+++ Miles per gallon to kilometers.js
@@ -1,16 +1,19 @@
 // Miles per gallon to kilometers per liter
-
 //code to convert miles per imperial gallon to kilometers per liter
-
 // Some useful associations relevant to this kata:
 
 // 1 Imperial Gallon = 4.54609188 litres
 // 1 Mile = 1.609344 kilometres
 
+//code to convert miles per imperial gallon to kilometers per liter
+
 
 
 function converter(mpg) {
-   return +(mpg * 0.354006).toFixed(2);
+   const iGallon = 4.54609188;
+   const mile = 1.609344;
+   const num = (mpg * (mile / iGallon)).toFixed(2);
+   return +num;
 }
 
 
