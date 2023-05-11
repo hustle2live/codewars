2338e13908d0850bbeee0982439a078d01f60ef0
hustle2live <kuz.v@ukr.net>
2023-05-11 13:12:12 +0300

    smallest unused ID moder

--- Algoorithms/8-KYU/Smallest unused ID.js
+++ Algoorithms/8-KYU/Smallest unused ID.js
@@ -1,9 +1,16 @@
-function nextId(ids) {
-   const arr = [];
-   for (let i = 1; i <= ids.length; i++) {
-      if (!ids.find((n) => n === i)) arr.push(+i);
-   }
-   const minNum = arr.sort((a, b) => b - a);
+// Hey awesome programmer!
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+// Go on and code some pure awesomeness!
 
-   return minNum.pop();
+
+
+
+function nextId(arr) {
+   for (let i = 0; i <= arr.length; i++) {
+      if (!arr.some((x) => x === i)) return i;
+   }
 }
+
+
