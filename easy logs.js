d8c850142ddfce0f020560f59fe04942bec9e615
hustle2live <kuz.v@ukr.net>
2023-05-11 12:39:43 +0300

    easy logs

---
+++ easy logs.js
@@ -0,0 +1,12 @@
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+// https://www.codewars.com/kata/5b68c7029756802aa2000176/train/javascript
+
+
+
+
+function logs(x, a, b) {
+   return (Math.log(a) + Math.log(b)) / Math.log(x);
+}
+
+
