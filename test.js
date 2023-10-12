264b0c8d8e01cbb18d3fedb45a57661414a2eb41
hustle2live <kuz.v@ukr.net>
2023-10-12 15:22:12 +0300

    test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+const arrA = [['dsd', 'ds'], 'dsfsf', 12344];
+const arrB = [
+   Symbol,
+   'asddds',
+   function ddd() {
+      console.log('ddd is done');
+   },
+   '2234',
+   { name: 'john', surname: 'picket' },
+   null,
+   undefined,
+   0,
+   ,
+   Symbol('x')
+];
+
+// console.log(arrA);
+// console.log(arrB);
+
+console.log(arrB);
