d11b6521f937f2b1c58ab5ab8846153268680220
hustle2live <kuz.v@ukr.net>
2023-10-12 13:56:34 +0300

    Center-yourself.js 2 versions

--- 7-kyu/Center-yourself.js
+++ 7-kyu/Center-yourself.js
@@ -6,3 +6,33 @@ function center(strng, width, fill = ' ') {
    const padLength = (width - strng.length) / 2;
    return fill.repeat(Math.ceil(padLength)) + strng + fill.repeat(Math.floor(padLength));
 }
+
+function centerNotRefactor(strng, width, fill = ' ') {
+   if (strng.length > width) {
+      return strng;
+   }
+
+   if (strng.length === 0 && width > 0) {
+      return [...Array(width).keys()].map((i) => fill).join('');
+   }
+
+   const fillLength = width - strng.length;
+
+   let padLeft = [];
+   let padRight = [];
+
+   if (fillLength % 2 === 0) {
+      newArrLength = fillLength / 2 || 0;
+
+      padLeft = padRight = [...Array(newArrLength).keys()].map((i) => fill);
+   } else {
+      newArrLength = Math.floor(fillLength / 2) + 1 || 0;
+
+      padLeft = [...Array(newArrLength).keys()].map((i) => fill);
+      padRight = padLeft.slice(0, -1);
+   }
+
+   const newString = padLeft.join('') + strng + padRight.join('');
+
+   return newString;
+}
