0becd7cd34607927408a2c4721f99bfc3849277e
volodymyr kuznietsov <kuz.v@ukr.net>
2024-04-08 19:08:05 +0300

    moderate bubble sort

--- freeCodeCamp/Algorithms/4_bubble_sort.js
+++ freeCodeCamp/Algorithms/4_bubble_sort.js
@@ -1,4 +1,5 @@
 function bubbleSort(array) {
+   let count = 0;
    for (let i = 0; i < array.length; i++) {
       for (let k = i + 1; k < array.length; k++) {
          if (array[i] > array[k]) {
@@ -6,11 +7,34 @@ function bubbleSort(array) {
             array[i] = array[k];
             array[k] = tmp;
          }
+         count += 1;
       }
    }
+   console.log('count ' + count);
+   return array;
+}
+
+function bubbleSortSec(array) {
+   let count = 0;
+   for (let i = 0; i < array.length; i++) {
+      for (let k = 0; k < array.length; k++) {
+         if (array[k + 1] < array[k]) {
+            let tmp = array[k];
+            array[k] = array[k + 1];
+            array[k + 1] = tmp;
+         }
+         count += 1;
+      }
+   }
+   console.log('countSec ' + count);
 
    return array;
 }
 
-console.log(bubbleSort([44, 12, 12, 8, 45, 13, 3, -2, 56, 31, 13, -63, 2, 55, 1, 23, 92]));
-console.log(bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
+// console.log(bubbleSort([44, 12, 12, 8, 45, 13, 3, -2, 56, 31, 13, -63, 2, 55, 1, 23, 92]));
+// console.log(bubbleSortSec([44, 12, 12, 8, 45, 13, 3, -2, 56, 31, 13, -63, 2, 55, 1, 23, 92]));
+
+console.log(
+   bubbleSort([44, 12, 12, 8, 45, 13, 3, -2, 56, 31, 13, -63, 2, 55, 1, 23, 92]).join(',') ===
+      bubbleSortSec([44, 12, 12, 8, 45, 13, 3, -2, 56, 31, 13, -63, 2, 55, 1, 23, 92]).join(',')
+);
