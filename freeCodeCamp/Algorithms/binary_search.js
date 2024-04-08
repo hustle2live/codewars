3b6c1de7c2e0a40e164d11a12fb3b9e717fe4168
volodymyr kuznietsov <kuz.v@ukr.net>
2024-04-08 15:00:38 +0300

    binary search

---
+++ freeCodeCamp/Algorithms/binary_search.js
@@ -0,0 +1,26 @@
+function binarySearch(list, item) {
+   let arrPath = [];
+   let found = false;
+   let low = 0;
+   let high = list.length - 1;
+
+   while (found === false && low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const guess = list[mid];
+
+      const checkEquality = (el = guess) => el === item;
+
+      if (checkEquality()) {
+         found = true;
+      } else if (guess > item) {
+         high = mid - 1;
+      } else {
+         low = mid + 1;
+      }
+
+      arrPath.push(guess);
+   }
+
+   return found ? arrPath : 'Value Not Found';
+}
+
